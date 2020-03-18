ff0b2cb84e5472c0d1d8ec1cac75b0b8c32ae9d4
krsh2001 <dynosa,ead@gmail.com>
2020-03-18 18:52:24 -0400

    added sheets functionality to script

--- data_generator_scripts/confirmed_cases/script.py
+++ data_generator_scripts/confirmed_cases/script.py
@@ -1,13 +1,63 @@
 import pandas as pd
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow,Flow
+from google.auth.transport.requests import Request
+import os
+import pickle
 import json
 from geopy.geocoders import Nominatim
 
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo'
+SAMPLE_RANGE_NAME = 'A1:AA1000'
+
+def main():
+    global values_input, service
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES) # here enter the name of your downloaded JSON file
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values_input = result_input.get('values', [])
+
+    if not values_input and not values_expansion:
+        print('No data found.')
+
+main()
+
+df=pd.DataFrame(values_input)
+
+last_updated = df[0][0]
+df = df.drop([0, 1])
+
+column_names = []
+
+for item in df.iloc[0]:
+    column_names.append(item)
+
+df.columns = column_names
+df.index = df.index - 2
+df = df.drop(0)
+
 geolocator = Nominatim(user_agent="COVIDScript")
-
-fields = ['health_region', 'province']
-
-df = pd.read_csv('Public_COVID-19_Canada.csv', skipinitialspace=True, usecols=fields)
-df.sort_values(by='health_region', inplace=True)
 
 output = {}
 
@@ -35,6 +85,5 @@ with open('confirmed.js', 'w') as outfile:
     output_string = json.dumps(real_output)
     output_string = output_string.replace("Vancouver Coastal", "Vancouver")
     output_string = output_string.replace("'", r"\'")
+    outfile.write("data_last_updated = '" + last_updated + "';\n")
     outfile.write("data_confirmed = '"+output_string + "';")
-
-
