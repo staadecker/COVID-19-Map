4e0a92ade5049ce187c12df722cbf2a1ae623881
Anastasia Razdaibiedina <37491405+arazd@users.noreply.github.com>
2020-03-19 17:44:26 -0400

    Update sample_data_generator.py

--- data_generator_scripts/in_isolation_sample/sample_data_generator.py
+++ data_generator_scripts/in_isolation_sample/sample_data_generator.py
@@ -12,10 +12,14 @@ with open('../postal_code_boundries/v2/postal_code_boundaries.json') as file:
     data = json.load(file)
 
     for key in data.keys():
+    # fill generated_data with: key -> [#self-isolating, #severe conditions, #mild conditions]
         if random.random() < 0.1:
-            generated_data[key] = random.randint(0, MAX_NUMBER)
+            num_isolated = random.randint(0, MAX_NUMBER)
+            num_severe = int(num_isolated*0.8)
+            num_mild = num_isolated - num_severe
+            generated_data[key] = (num_isolated, num_severe, num_mild)
         else:
-            generated_data[key] = 0
+            generated_data[key] = (0,0,0)
 
 full_data = {"time": time.time(), "max": MAX_NUMBER, "fsa": generated_data}
 
