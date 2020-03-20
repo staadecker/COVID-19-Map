f60f9f5c599c5ad377a1c8a2f7473f5eae12e119
krsh2001 <dynosa,ead@gmail.com>
2020-03-20 00:55:00 -0400

    added current loation popup

--- location.js
+++ location.js
@@ -5,6 +5,7 @@ if (navigator.geolocation) {
       longit = position.coords.longitude;
       // this is just a marker placed in that position
       var abc = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+      abc.bindPopup("<h3>Current Location</h3>");
       // move the map to have the location in its center
       map.setView(new L.LatLng(latit, longit), 10);
     });
