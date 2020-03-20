8e69021ef0076ac6365b5ed7f108b85dc9b47022
Martin Staadecker <machstg@gmail.com>
2020-03-20 02:03:32 -0400

    Go back to switch statement in toggle.js

--- pages/toggle.js
+++ pages/toggle.js
@@ -1,17 +1,20 @@
 function toggle_clicked(radio) {
-    if (radio.value === "in_self_isolation") {
-        map.removeLayer(confirmedCircles);
-        selfIsolatedPolygons.addTo(map);
-        map.removeLayer(highRiskPolygons);
-    } else if (radio.value === "high_risk") {
-        map.removeLayer(confirmedCircles);
-        map.removeLayer(selfIsolatedPolygons);
-        highRiskPolygons.addTo(map);
-    } else {
-        confirmedCircles.addTo(map);
-        map.removeLayer(selfIsolatedPolygons);
-        map.removeLayer(highRiskPolygons);
-
+    switch (radio.value) {
+        case "in_self_isolation":
+            map.removeLayer(confirmedCircles);
+            selfIsolatedPolygons.addTo(map);
+            map.removeLayer(highRiskPolygons);
+            break;
+        case "high_risk":
+            map.removeLayer(confirmedCircles);
+            map.removeLayer(selfIsolatedPolygons);
+            highRiskPolygons.addTo(map);
+            break;
+        default:
+            confirmedCircles.addTo(map);
+            map.removeLayer(selfIsolatedPolygons);
+            map.removeLayer(highRiskPolygons);
     }
+
     current_location.bringToFront();
 }
