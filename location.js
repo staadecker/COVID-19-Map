7528ee41f83f0c0b879710a8a933071a05cf4c09
krsh2001 <dynosa,ead@gmail.com>
2020-03-19 23:20:03 -0400

    ported to leaflet map

--- location.js
+++ location.js
@@ -1,22 +1,9 @@
 // Try HTML5 geolocation. // Else Browser doesn't support Geolocation or permission not given.
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-            const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            const marker = new google.maps.Marker({
-                position: pos,
-                icon: {
-                    path: google.maps.SymbolPath.CIRCLE,
-                    scale: 8,
-                    strokeWeight: 3,
-                    fillColor: 'royalblue',
-                    strokeColor: 'white',
-                    fillOpacity: 1,
-                    strokeOpacity: 0.5
-                },
-            });
+            const pos = L.LatLng(position.coords.latitude, position.coords.longitude);
+            
+            const marker = new L.marker(pos).addTo(map);
 
             map.setCenter(pos);
             map.setZoom(8);
