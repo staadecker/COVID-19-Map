af1f1954b35977cc51dfb75d0c770302491130cb
leglesslamb <wu.rupert@outlook.com>
2020-03-19 21:19:20 -0400

    Added high risk generator and heatmap rendering; changed naming and colours in script.js.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Create map
+// Create map.
 map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(53.9902, -97.8155),
     zoom: 4,
@@ -38,51 +38,57 @@ if (navigator.geolocation) {
 // Load data files
 postal_code_data = JSON.parse(data_postal_code_boundaries);
 in_self_isolation_data = JSON.parse(data_in_self_isolation_sample);
+high_risk_data = JSON.parse(data_high_risk_sample);
 confirmed_data = JSON.parse(data_confirmed);
 
-// Create layers
-
-//Array of Google Map API polygons and markers
-let polygons = [];
-let markers = [];
-
+// Array of Google Map API polygons for self-isolated and high-risk addresses.
 let polygonCount = 0;
+let selfIsolatedPolygons = [];
+let highRiskPolygons = [];
 for (let fsa in postal_code_data) {
-    if (postal_code_data.hasOwnProperty(fsa)) {
-        const num_in_self_isolation = in_self_isolation_data['fsa'][fsa];
+    if (!postal_code_data.hasOwnProperty(fsa)) continue
+    const num_in_self_isolation = in_self_isolation_data['fsa'][fsa];
+    const num_high_risk = high_risk_data['fsa'][fsa];
 
-        for (let i = 0; i < postal_code_data[fsa].length; i++) {
+    for (let i = 0; i < postal_code_data[fsa].length; i++) {
 
+        // Add the polygons.
+        const selfIsolatedPolygon = new google.maps.Polygon({
+            paths: postal_code_data[fsa][i]['coord'],
+            strokeWeight: 0.5,
+            fillColor: '#FF8800',
+            fillOpacity: num_in_self_isolation / in_self_isolation_data['max'] * 0.5,
+            indexID: polygonCount
+        });
+        const highRiskPolygon = new google.maps.Polygon({
+            paths: postal_code_data[fsa][i]['coord'],
+            strokeWeight: 0.5,
+            fillColor: '#FFCC00',
+            fillOpacity: num_high_risk / high_risk_data['max'] * 0.5,
+            indexID: polygonCount
+        });
 
-            //Add the polygon
-            const p = new google.maps.Polygon({
-                paths: postal_code_data[fsa][i]['coord'],
-                strokeWeight: 0.5,
-                fillColor: '#FF0000',
-                fillOpacity: num_in_self_isolation / in_self_isolation_data['max'] * 0.5,
-                indexID: polygonCount
-            });
+        // Initialize infowindow text
+        selfIsolatedPolygon.info = new google.maps.InfoWindow({ /*maxWidth : 250,*/
+            content: "<h3>" + fsa + "</h3><p>" + num_in_self_isolation + " people in self-isolation</p>"
+        });
+        highRiskPolygon.info = new google.maps.InfoWindow({ /*maxWidth : 250,*/
+            content: "<h3>" + fsa + "</h3><p>" + num_high_risk + " people at high risk</p>"
+        });
 
+        // Add polygons to polygon arrays and add click listeners.
+        selfIsolatedPolygons[polygonCount] = selfIsolatedPolygon;
+        highRiskPolygons[polygonCount] = highRiskPolygon;
+        selfIsolatedPolygon.addListener('click', item_pressed);
+        highRiskPolygon.addListener('click', item_pressed)
 
-            //Initialize infowindow text
-            p.info = new google.maps.InfoWindow({
-                /*maxWidth : 250,*/
-                content: "<h3>" + fsa + "</h3><p>" + num_in_self_isolation + " people in self-isolation</p>"
-            });
-
-            //Add polygon to polygon array
-            polygons[polygonCount] = p;
-
-            //Runs when user clicks on polygon
-            p.addListener('click', item_pressed);
-
-
-            polygonCount++;
-        }
+        polygonCount++;
     }
 }
 
-//Loop on the confirmed cases
+
+// Array of Google Map API markers for confirmed cases.
+let confirmedMarkers = [];
 for (let i = 0; i < confirmed_data.length; i++) {
     //Add the marker
     const position = new google.maps.LatLng(confirmed_data[i].coord[0], confirmed_data[i].coord[1]);
@@ -92,48 +98,49 @@ for (let i = 0; i < confirmed_data.length; i++) {
     });
 
     //initialize infowindow text
-    marker.info = new google.maps.InfoWindow({
-        //maxWidth : 250,
+    marker.info = new google.maps.InfoWindow({ // maxWidth : 250,
         content: "<h3>" + confirmed_data[i].name + "</h3><p>" + confirmed_data[i].cases + " confirmed cases in this area</p>"
     });
 
-    //Add polygon to polygon array
-    markers[i] = marker;
-
-    //Runs when user clicks on polygon
+    // Add polygon to polygon array and add click listener.
+    confirmedMarkers[i] = marker;
     marker.addListener('click', item_pressed);
 }
 
 // Enable marker layer
-setMapOnAll(map, markers);
-
-//Functions
-
+setMapOnAll(map, confirmedMarkers);
 
 function item_pressed(event) {
-    //Close all info windows
-    for (let i = 0; i < polygons.length; i++) {
-        polygons[i].info.close();
+    // Close all info windows.
+    for (let i = 0; i < polygonCount; i++) {
+        selfIsolatedPolygons[i].info.close();
+        highRiskPolygons[i].info.close();
     }
-    for (let i = 0; i < markers.length; i++) {
-        markers[i].info.close();
+    for (let i = 0; i < confirmedMarkers.length; i++) {
+        confirmedMarkers[i].info.close();
     }
 
-    //Open polygon infowindow
+    // Open polygon infowindow.
     this.info.setPosition(event.latLng);
     this.info.open(map, this);
 }
 
 function toggle_clicked(radio) {
-    if (radio.value === "in_self_isolation") {
-        setMapOnAll(null, markers);
-        setMapOnAll(map, polygons);
-    } else {
-        setMapOnAll(map, markers);
-        setMapOnAll(null, polygons);
+    switch (radio.value) {
+        case "in_self_isolation": 
+            setMapOnAll(null, confirmedMarkers);
+            setMapOnAll(map, selfIsolatedPolygons);
+            setMapOnAll(null, highRiskPolygons); break;
+        case "high_risk": 
+            setMapOnAll(null, confirmedMarkers);
+            setMapOnAll(null, selfIsolatedPolygons);
+            setMapOnAll(map, highRiskPolygons); break;
+        default: 
+            setMapOnAll(map, confirmedMarkers);
+            setMapOnAll(null, selfIsolatedPolygons);
+            setMapOnAll(null, highRiskPolygons); break;
     }
 }
-
 
 // Set every item in group to the map specified by map. map can be null
 function setMapOnAll(map, groups) {
