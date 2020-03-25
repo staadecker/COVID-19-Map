a6aa19447b010f78670e662345c18c86481995b2
leglesslamb <wu.rupert@outlook.com>
2020-03-24 21:26:39 -0400

    Refactored frontend, reduced some arrow code.

--- website/map.js
+++ website/map.js
@@ -13,9 +13,7 @@ var bucket;
 // 2. Create map.
 const canada_bounds = [[38, -150], [87, -45]];
 
-const map = new L.map('map', {
-    'maxBounds': canada_bounds
-});
+const map = new L.map('map', {'maxBounds': canada_bounds});
 
 const tiles = new L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -32,17 +30,34 @@ const postal_code_data = JSON.parse(data_postal_code_boundaries);
 let confirmedCircles, selfIsolatedPolygons, highRiskPolygons, selfIso_legend, highRisk_legend;
 let form_data_obj, confirmed_data;
 
+function getColour_selfIso(cases) {
+    return getColour(cases, ['#800026', '#BD0026', '#E31A1C', '#FC4E2A', '#FD8D3C', '#FEB24C', '#FED976', '#FFEDA0']);
+}
+
+function getColour_highRisk(cases) {
+    return getColour(cases, ['#2e012d', '#620147', '#a81c6f', '#f32074', '#f6577d', '#f98378', '#fa9e95', '#ffc4a7']);
+}
+
+function getColour(cases, colours) {
+    let thresholds = [1000, 500, 200, 100, 50, 20, 10, -1];
+    for (let i = 0; i < Math.max(thresholds.length, colours.length); i++) {
+        if (cases > thresolds[i]) return colours[i];
+    }
+    return colours[colours.length-1];
+}
+
 function displayMaps() {
     // 1. Create the layers
-    // Array of Google Map API polygons for self-isolated and high-risk addresse
+
+    // Array of Google Map API polygons for self-isolated and high-risk addresses.
     selfIsolatedPolygons = L.layerGroup();
     highRiskPolygons = L.layerGroup();
 
-    // For each postal code in Canada
+    // For each postal code in Canada.
     for (let fsa in postal_code_data) {
         if (!postal_code_data.hasOwnProperty(fsa)) continue;
 
-        //2. Get the number of reports
+        // 2. Get the number of reports.
         let num_potential = 0;
         let num_high_risk = 0;
         let total_reports_region = 0;
@@ -52,112 +67,72 @@ function displayMaps() {
             total_reports_region = form_data_obj['fsa'][fsa]['number_reports'];
         }
 
-        // Get the colors
-        const colour_selfIso = getColor_selfIso(num_potential);
-        const colour_highRisk = getColor_highRisk(num_high_risk);
+        // Get the Colours.
+        const colour_selfIso = getColour_selfIso(num_potential);
+        const colour_highRisk = getColour_highRisk(num_high_risk);
 
-        let opacity_selfIso = 0.4;
-        let opacity_highRisk = 0.4;
+        let opacity_selfIso = (num_potential === 0) ? 0: 0.4;
+        let opacity_highRisk = (num_high_risk === 0) ? 0: 0.4;
 
         let msg_selfIso = "<h3>" + fsa + "</h3><p>We received " + num_potential + " reports from potential cases.</p><p>We received " + total_reports_region + " reports in total.</p>";
         let msg_highRisk = "<h3>" + fsa + "</h3><p>We received " + num_high_risk + " reports from vulnerable individuals.</p><p>We received " + total_reports_region + " reports in total.</p>";
-
-        if (num_potential === 0) {
-            opacity_selfIso = 0;
-        }
-
-        if (num_high_risk === 0) {
-            opacity_highRisk = 0;
-        }
 
         if (total_reports_region === 0) {
             msg_selfIso = "<h3>" + fsa + "</h3><p>We haven't had enough form responses in this region yet.</p>";
             msg_highRisk = "<h3>" + fsa + "</h3><p>We haven't had enough form responses in this region yet.</p>";
         }
 
-        for (let i = 0; i < postal_code_data[fsa].length; i++) {
-            // Add the polygons.
-            const selfIsolatedPolygon = new L.Polygon(postal_code_data[fsa][i]['coord'], {
-                weight: 0.9,
-                color: 'gray',
-                dashArray: '3',
-                fillColor: colour_selfIso,
-                fillOpacity: opacity_selfIso,
-            });
-
-            const highRiskPolygon = new L.Polygon(postal_code_data[fsa][i]['coord'], {
-                weight: 0.9,
-                color: 'gray',
-                dashArray: '3',
-                fillColor: colour_highRisk,
-                fillOpacity: opacity_highRisk,
-            });
-
-
-            //Initialize infowindow text
+        postal_code_data[fsa].forEach(function (datum) {
+            // Initialize infowindow text.
             selfIsolatedPolygon.bindPopup(msg_selfIso);
             highRiskPolygon.bindPopup(msg_highRisk);
 
-            // Add polygons to polygon arrays and add click listeners.
-            selfIsolatedPolygon.addTo(selfIsolatedPolygons);
-            highRiskPolygon.addTo(highRiskPolygons);
-        }
+            // Create polygons, add click listeners and add to polygon arrays.
+            selfIsolatedPolygon.addTo(new L.Polygon(datum['coord'], {
+                weight: 0.9,
+                Colour: 'gray',
+                dashArray: '3',
+                fillColour: colour_selfIso,
+                fillOpacity: opacity_selfIso,
+            }));
+            highRiskPolygon.addTo(new L.Polygon(datum['coord'], {
+                weight: 0.9,
+                Colour: 'gray',
+                dashArray: '3',
+                fillColour: colour_highRisk,
+                fillOpacity: opacity_highRisk,
+            }));
+        });
     }
 
-    function getColor_selfIso(cases) {
-        return cases > 1000 ? '#800026' :
-            cases > 500 ? '#BD0026' :
-                cases > 200 ? '#E31A1C' :
-                    cases > 100 ? '#FC4E2A' :
-                        cases > 50 ? '#FD8D3C' :
-                            cases > 20 ? '#FEB24C' :
-                                cases > 10 ? '#FED976' :
-                                    '#FFEDA0';
-    }
-
-    function getColor_highRisk(cases) {
-        return cases > 1000 ? '#2e012d' :
-            cases > 500 ? '#620147' :
-                cases > 200 ? '#a81c6f' :
-                    cases > 100 ? '#f32074' :
-                        cases > 50 ? '#f6577d' :
-                            cases > 20 ? '#f98378' :
-                                cases > 10 ? '#fa9e95' :
-                                    '#ffc4a7';
-    }
-
-    //Legend for self-isolated cases
+    // Legend for self-isolated cases.
     selfIso_legend = L.control({position: 'bottomright'});
-
     selfIso_legend.onAdd = function (map) {
-
         const div = L.DomUtil.create('div', 'info legend'),
             grades = [1, 10, 20, 50, 100, 200, 500, 1000],
-            labels = [];
 
-        // loop through our density intervals and generate a label with a colored square for each interval
+        /*  Loop through our density intervals and generate a label with a
+            Coloured square for each interval. */
         for (let i = 0; i < grades.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + getColor_selfIso(grades[i] + 1) + '"></i> ' +
+                '<i style="background:' + getColour_selfIso(grades[i] + 1) + '"></i> ' +
                 grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
         }
 
         return div;
     };
 
-    //Legend for high risk cases
+    // Legend for high risk cases.
     highRisk_legend = L.control({position: 'bottomright'});
 
     highRisk_legend.onAdd = function (map) {
-
         const div = L.DomUtil.create('div', 'info legend'),
-            grades = [1, 10, 20, 50, 100, 200, 500, 1000],
-            labels = [];
+        grades = [1, 10, 20, 50, 100, 200, 500, 1000],
 
-        // loop through our density intervals and generate a label with a colored square for each interval
+        // Loop through our density intervals and generate a label with a Coloured square for each interval.
         for (let i = 0; i < grades.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + getColor_highRisk(grades[i] + 1) + '"></i> ' +
+                '<i style="background:' + getColour_highRisk(grades[i] + 1) + '"></i> ' +
                 grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
         }
 
@@ -170,43 +145,41 @@ function displayMaps() {
     const max_rad = 35;
     let confirmed_cases_data = confirmed_data['confirmed_cases'];
     for (let i = 0; i < confirmed_cases_data.length; i++) {
-        //Add the marker
-        if (confirmed_cases_data[i]['coord'][0] !== "N/A") {
-            let rad;
-            if (confirmed_cases_data[i]['cases'] < 10) {
-                rad = 5;
-            } else {
-                rad = 5 + confirmed_cases_data[i]['cases'] / confirmed_data['max_cases'] * max_rad;
-            }
+        if (confirmed_cases_data[i]['coord'][0] === "N/A") continue;
 
-            const circle = new L.circleMarker(confirmed_cases_data[i]['coord'], {
-                weight: 0,
-                color: 'red',
-                fillColor: '#f03',
-                fillOpacity: 0.5,
-                radius: rad
-            });
-
-            // create the popup text and bind to the correct circle.
-            // store popup index as a member of the popup so that we can set the popup to be
-            // in the centre of the circle on callback when clicked
-            let text = "<h3>" + confirmed_cases_data[i].name + "</h3><p>" + confirmed_cases_data[i]['cases'] + " confirmed cases in this area</p>";
-            let popup = L.popup().setLatLng(confirmed_cases_data[i]['coord']).setContent(text);
-            popup.popup_idx = i;
-
-            circle.bindPopup(popup);
-
-            //Add circle to circle array
-            circle.addTo(confirmedCircles);
+        // Add the marker.
+        let rad = 5;
+        if (confirmed_cases_data[i]['cases'] >= 10) {
+            rad += confirmed_cases_data[i]['cases'] / confirmed_data['max_cases'] * max_rad;
         }
+
+        const circle = new L.circleMarker(confirmed_cases_data[i]['coord'], {
+            weight: 0,
+            Colour: 'red',
+            fillColour: '#f03',
+            fillOpacity: 0.5,
+            radius: rad
+        });
+
+        /*  Create the popup text and bind to the correct circle. Store popup
+            index as a member of the popup so that we can set the popup to be
+            in the centre of the circle on callback when clicked. */
+        let text = "<h3>" + confirmed_cases_data[i].name + "</h3><p>" +
+            confirmed_cases_data[i]['cases'] + " confirmed cases in this area</p>";
+
+        let popup = L.popup().setLatLng(confirmed_cases_data[i]['coord']).setContent(text);
+        popup.popup_idx = i;
+
+        //Bind popup and add circle to circle array.
+        circle.bindPopup(popup);
+        circle.addTo(confirmedCircles);
     }
 }
 
-// sets the pop up to be in the center of the circle when you click on it
+// Sets the popup to be in the center of the circle when you click on it.
 map.on("popupopen", function (event) {
-    if (typeof (event.popup.popup_idx) != 'undefined') {
+    if (typeof (event.popup.popup_idx) != 'undefined')
         event.popup.setLatLng(confirmed_data['confirmed_cases'][event.popup.popup_idx]['coord']);
-    }
 });
 
 function getGSDownloadURL(bucket_reference, file) {
@@ -245,6 +218,6 @@ async function obtainAndDisplayMaps() {
     toggle_clicked("potential");
 }
 
-// Calls the function
+// Calls the function.
 obtainAndDisplayMaps();
 
--- website/modal.js
+++ website/modal.js
@@ -1,20 +1,20 @@
-// Get the modal
+// Get the modal.
 const modal1 = document.getElementById("myModal1");
 const modal2 = document.getElementById("myModal2");
 const modal3 = document.getElementById("myModal3");
 
-// Get the button that opens the modal
+// Get the button that opens the modal.
 const btn1 = document.getElementById("myBtn1");
 const btn2 = document.getElementById("myBtn2");
 const btn3 = document.getElementById("myBtn3");
 
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal.
+// var span = document.getElementsByClassName("close")[0];
 const span1 = document.getElementById("close1");
 const span2 = document.getElementById("close2");
 const span3 = document.getElementById("close3");
 
-// When the user clicks the button, open the modal
+// When the user clicks the button, open the modal.
 btn1.onclick = function() {
     modal1.style.display = "block";
 };
@@ -28,7 +28,7 @@ btn3.onclick = function() {
 };
 
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modal.
 span1.onclick = function() {
     modal1.style.display = "none";
 };
@@ -41,17 +41,11 @@ span3.onclick = function() {
     modal3.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it.
 window.onclick = function(event) {
-    if (event.target === modal1) {
-        modal1.style.display = "none";
-    }
-
-    if (event.target === modal2) {
-        modal2.style.display = "none";
-    }
-
-    if (event.target === modal3) {
-        modal3.style.display = "none";
+    switch (event.target) {
+        case modal1: modal1.style.display = "none"; break;
+        case modal2: modal2.style.display = "none"; break;
+        case modal3: modal3.style.display = "none"; break;
     }
 };
