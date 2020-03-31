b8b026219c273d6206344d848bf4f631d3ddb85e
leglesslamb <wu.rupert@outlook.com>
2020-03-31 16:15:27 -0400

    Tidied toggle.js.

--- javascripts/toggle.js
+++ javascripts/toggle.js
@@ -1,34 +1,37 @@
 function toggle_clicked(radioValue) {
     switch (radioValue) {
         case "confirmed":
-            confirmedCircles.addTo(map);
             map.removeLayer(selfIsolatedPolygons);
             map.removeLayer(highRiskPolygons);
-            document.getElementById("update_time").innerHTML = confirmed_data['last_updated'];
             map.removeControl(selfIso_legend);
             map.removeControl(highRisk_legend);
+
+            confirmedCircles.addTo(map);
+
+            document.getElementById("update_time").innerHTML = confirmed_data['last_updated'];
             break;
 
         case "vulnerable":
             map.removeLayer(confirmedCircles);
             map.removeLayer(selfIsolatedPolygons);
+            if (map.legend !== null) map.removeControl(selfIso_legend);
+
             highRiskPolygons.addTo(map);
             highRisk_legend.addTo(map);
-            if (!(map.legend === null)) map.removeControl(selfIso_legend);
 
             document.getElementById("update_time").innerHTML = "Total Responses: " + form_data_obj['total_responses'] + " | Last update: " + new Date(1000 * form_data_obj["time"]);
             break;
 
         case "potential":
             map.removeLayer(confirmedCircles);
-            selfIsolatedPolygons.addTo(map);
             map.removeLayer(highRiskPolygons);
+            if (map.legend !== null) map.removeControl(highRisk_legend);
+            
+            selfIsolatedPolygons.addTo(map);
             selfIso_legend.addTo(map);
-            if (!(map.legend === null)) map.removeControl(highRisk_legend);
 
             document.getElementById("update_time").innerHTML = "Total Responses: " + form_data_obj['total_responses'] + " | Last update: " + new Date(1000 * form_data_obj["time"]);
             break;
-
         default:
             console.log("Toggle called with the wrong option. " + radioValue)
     }
