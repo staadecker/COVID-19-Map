ea2ff1afe6aec5cacf766fe2852abcdfa97ff7d1
Martin Staadecker <machstg@gmail.com>
2020-04-01 19:20:22 -0400

    Text loader outline

---
+++ javascripts/text_loader.js
@@ -0,0 +1,8 @@
+// 1. Get language from parameter in URL
+
+// 2. Load language file
+
+// 3. For each key (ex. "about-us-paragraph-3")
+    // 4. Get id by prefixing lan- (ex. "lan-about-us-paragraph")
+    // 5. Get element with that id
+    // 6. Set element text to the value in the dictionary
